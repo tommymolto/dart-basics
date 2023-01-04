359e55779e9087910191972fa2460c0f11a2027c
Paulo Marinho <tommymolto@gmail.com>
2023-01-04 19:26:41 -0300

    Versao funcional

--- bin/alunos.dart
+++ bin/alunos.dart
@@ -1,5 +1,34 @@
 import 'package:alunos/alunos.dart' as alunos;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${alunos.calculate()}!');
+  String entrada = '';
+  while (entrada != '0') {
+    stdout.writeln("Digite o que deseja : ");
+    stdout.writeln("1 para novos alunos  ");
+    stdout.writeln("2 para a media da turma  ");
+    stdout.writeln("0 para sair do aplicativo  ");
+    entrada = stdin.readLineSync().toString();
+    switch (entrada) {
+      case '1':
+        alunos.cadastraAlunos();
+        break;
+      case '2':
+        alunos.calculaMedia();
+        break;
+      case '0':
+        break;
+      default:
+        alunos.naoIdentificado();
+    }
+    //se entrada =1
+    // chama cadastro de usuarios
+    //se entrada = 2
+    // mostra media de alunos
+    //senao
+    // fala que nao entendeu o inserido
+    // se 0, sai do aplicativo
+
+    //stdout.write(entrada);
+  }
 }
--- lib/alunos.dart
+++ lib/alunos.dart
@@ -1,3 +1,27 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+final Map<String, double> baseAlunos = <String, double>{};
+cadastraAlunos() {
+  stdout.writeln('Entre com o nome do aluno:');
+  final nomealuno = stdin.readLineSync().toString();
+  stdout.writeln('Entre com a nota do aluno:');
+  final notaaluno = stdin.readLineSync().toString();
+  baseAlunos[nomealuno] = double.tryParse(notaaluno) ?? 0;
+  stdout.writeln('Ja temos ${baseAlunos.length} alunos');
+
+  stdout.writeln('O aluno $nomealuno foi cadastrado com a nota $notaaluno');
+}
+
+calculaMedia() {
+  //final total = baseAlunos.map((chave, valor) => valor).
+  double total = 0;
+  baseAlunos.forEach((key, value) {
+    total += baseAlunos[key]!;
+  });
+  final media = total / baseAlunos.length;
+  stdout.writeln('A media é $media!');
+}
+
+naoIdentificado() {
+  stdout.writeln('Não entendi a opção que você deseja!');
 }
