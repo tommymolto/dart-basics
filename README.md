97aa74b3fb74b04806d50b4875b904c693019363
Paulo Marinho <tommymolto@gmail.com>
2023-01-04 18:46:28 -0300

    01-projeto vazio

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projeto de linha de comando dart
+
+Funcionalidades:
+
+* Cadastro de Aluno e nota final
+* Geração de media
---
+++ bin/alunos.dart
@@ -0,0 +1,5 @@
+import 'package:alunos/alunos.dart' as alunos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alunos.calculate()}!');
+}
